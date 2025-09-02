ec35a8e527359ba80908835240ea1b6d5edbbac4
1ibra <82260531+1ibra@users.noreply.github.com>
2025-09-02 22:07:34 +0300

    Update app.js

--- apps/antonclk/app.js
+++ apps/antonclk/app.js
@@ -1,119 +1,101 @@
-
 // Clock with large digits using the "Anton" bold font
+
 Graphics.prototype.setFontAnton = function (scale) {
     // Actual height 69 (68 - 0)
     g.setFontCustom(atob("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADgAAAAAAAAAAA/gAAAAAAAAAAP/gAAAAAAAAAH//gAAAAAAAAB///gAAAAAAAAf///gAAAAAAAP////gAAAAAAD/////gAAAAAA//////gAAAAAP//////gAAAAH///////gAAAB////////gAAAf////////gAAP/////////gAD//////////AA//////////gAA/////////4AAA////////+AAAA////////gAAAA///////wAAAAA//////8AAAAAA//////AAAAAAA/////gAAAAAAA////4AAAAAAAA///+AAAAAAAAA///gAAAAAAAAA//wAAAAAAAAAA/8AAAAAAAAAAA/AAAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////AAAAAB///////8AAAAH////////AAAAf////////wAAA/////////4AAB/////////8AAD/////////+AAH//////////AAP//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wA//8AAAAAB//4A//wAAAAAAf/4A//gAAAAAAP/4A//gAAAAAAP/4A//gAAAAAAP/4A//wAAAAAAf/4A///////////4Af//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH//////////AAD/////////+AAB/////////8AAA/////////4AAAP////////gAAAD///////+AAAAAf//////4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAP/gAAAAAAAAAAP/gAAAAAAAAAAf/gAAAAAAAAAAf/gAAAAAAAAAAf/AAAAAAAAAAA//AAAAAAAAAAA/+AAAAAAAAAAB/8AAAAAAAAAAD//////////gAH//////////gAP//////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH/4AAAAB/gAAD//4AAAAf/gAAP//4AAAB//gAA///4AAAH//gAB///4AAAf//gAD///4AAA///gAH///4AAD///gAP///4AAH///gAP///4AAP///gAf///4AAf///gAf///4AB////gAf///4AD////gA////4AH////gA////4Af////gA////4A/////gA//wAAB/////gA//gAAH/////gA//gAAP/////gA//gAA///8//gA//gAD///w//gA//wA////g//gA////////A//gA///////8A//gA///////4A//gAf//////wA//gAf//////gA//gAf/////+AA//gAP/////8AA//gAP/////4AA//gAH/////gAA//gAD/////AAA//gAB////8AAA//gAA////wAAA//gAAP///AAAA//gAAD//8AAAA//gAAAP+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB/+AAAAAD/wAAB//8AAAAP/wAAB///AAAA//wAAB///wAAB//wAAB///4AAD//wAAB///8AAH//wAAB///+AAP//wAAB///+AAP//wAAB////AAf//wAAB////AAf//wAAB////gAf//wAAB////gA///wAAB////gA///wAAB////gA///w//AAf//wA//4A//AAA//wA//gA//AAAf/wA//gB//gAAf/wA//gB//gAAf/wA//gD//wAA//wA//wH//8AB//wA///////////gA///////////gA///////////gA///////////gAf//////////AAf//////////AAP//////////AAP/////////+AAH/////////8AAH///+/////4AAD///+f////wAAA///8P////gAAAf//4H///+AAAAH//gB///wAAAAAP4AAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/wAAAAAAAAAA//wAAAAAAAAAP//wAAAAAAAAB///wAAAAAAAAf///wAAAAAAAH////wAAAAAAA/////wAAAAAAP/////wAAAAAB//////wAAAAAf//////wAAAAH///////wAAAA////////wAAAP////////wAAA///////H/wAAA//////wH/wAAA/////8AH/wAAA/////AAH/wAAA////gAAH/wAAA///4AAAH/wAAA//+AAAAH/wAAA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gA///////////gAAAAAAAAH/4AAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAH/wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB//8AAA/////+B///AAA/////+B///wAA/////+B///4AA/////+B///8AA/////+B///8AA/////+B///+AA/////+B////AA/////+B////AA/////+B////AA/////+B////gA/////+B////gA/////+B////gA/////+A////gA//gP/gAAB//wA//gf/AAAA//wA//gf/AAAAf/wA//g//AAAAf/wA//g//AAAA//wA//g//gAAA//wA//g//+AAP//wA//g////////gA//g////////gA//g////////gA//g////////gA//g////////AA//gf///////AA//gf//////+AA//gP//////+AA//gH//////8AA//gD//////4AA//gB//////wAA//gA//////AAAAAAAH////8AAAAAAAA////AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//////gAAAAB///////+AAAAH////////gAAAf////////4AAB/////////8AAD/////////+AAH//////////AAH//////////gAP//////////gAP//////////gAf//////////wAf//////////wAf//////////wAf//////////wAf//////////4A//wAD/4AAf/4A//gAH/wAAP/4A//gAH/wAAP/4A//gAP/wAAP/4A//gAP/4AAf/4A//wAP/+AD//4A///wP//////4Af//4P//////wAf//4P//////wAf//4P//////wAf//4P//////wAP//4P//////gAP//4H//////gAH//4H//////AAH//4D/////+AAD//4D/////8AAB//4B/////4AAA//4A/////wAAAP/4AP////AAAAB/4AD///4AAAAAAAAAH/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//AAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAAAAA//gAAAAAAADgA//gAAAAAAP/gA//gAAAAAH//gA//gAAAAB///gA//gAAAAP///gA//gAAAD////gA//gAAAf////gA//gAAB/////gA//gAAP/////gA//gAB//////gA//gAH//////gA//gA///////gA//gD///////gA//gf///////gA//h////////gA//n////////gA//////////gAA/////////AAAA////////wAAAA///////4AAAAA///////AAAAAA//////4AAAAAA//////AAAAAAA/////4AAAAAAA/////AAAAAAAA////8AAAAAAAA////gAAAAAAAA///+AAAAAAAAA///4AAAAAAAAA///AAAAAAAAAA//4AAAAAAAAAA/+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//gB///wAAAAP//4H///+AAAA///8P////gAAB///+f////4AAD///+/////8AAH/////////+AAH//////////AAP//////////gAP//////////gAf//////////gAf//////////wAf//////////wAf//////////wA///////////wA//4D//wAB//4A//wB//gAA//4A//gA//gAAf/4A//gA//AAAf/4A//gA//gAAf/4A//wB//gAA//4A///P//8AH//4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////gAP//////////gAP//////////AAH//////////AAD/////////+AAD///+/////8AAB///8f////wAAAf//4P////AAAAH//wD///8AAAAA/+AAf//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH//gAAAAAAAAB///+AA/+AAAAP////gA//wAAAf////wA//4AAB/////4A//8AAD/////8A//+AAD/////+A///AAH/////+A///AAP//////A///gAP//////A///gAf//////A///wAf//////A///wAf//////A///wAf//////A///wA///////AB//4A//4AD//AAP/4A//gAB//AAP/4A//gAA//AAP/4A//gAA/+AAP/4A//gAB/8AAP/4A//wAB/8AAf/4Af//////////wAf//////////wAf//////////wAf//////////wAf//////////wAP//////////gAP//////////gAH//////////AAH/////////+AAD/////////8AAB/////////4AAAf////////wAAAP////////AAAAB///////4AAAAAD/////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAf/AAB/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAA//AAD/8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="), 46, atob("EiAnGicnJycnJycnEw=="), 78 + (scale << 8) + (1 << 16));
 };
 
-{
-    const Storage = require("Storage");
+let Storage = require("Storage");
+// === إعدادات عامة ===
+let settings = Storage.readJSON('setting.json', 1);
+let city = settings.city; // ضع رمز مدينتك: "makkah", "jeddah", "madinah", ...
+function yearFile(year) { // 1..12
+    return "uaq-" + city.toLowerCase() + "-" + year + ".json";
+}
+// يعيد {1:[hh:mm*6],2:[...],...} أو null
+function loadYear(year) {
+    let fn = yearFile(year);
+    try {
+        let obj = Storage.readJSON(fn);
+        return obj || null;
+    } catch (e) {
+        return null;
+    }
+}
 
-    // === إعدادات عامة ===
-    const YEAR = 2025; // عدّلها للسنة المطلوبة
-    const CITY = "JEDDAH"; // ضع رمز مدينتك: "makkah", "jeddah", "madinah", ...
+function todayYMD(d) {
+    return { y: d.getFullYear(), m: d.getMonth() + 1, day: d.getDate() };
+}
 
-    let yearFile = function (year) { // 1..12
-        return "uaq-" + CITY.toLowerCase() + "-" + year + ".json";
+function getTimesFor(d) {
+    let ymd = todayYMD(d);
+    let data = loadYear(ymd.y);
+    if (!data) return null;
+    let now = new Date();
+    let start = new Date(now.getFullYear(), 0, 0);
+    let diff = now - start;
+    let oneDay = 1000 * 60 * 60 * 24;
+    ymd.day = Math.floor(diff / oneDay);
+    let arr = data[ymd.day];
+    let nextArr = data[ymd.day+1];
+    if (!arr) return null;
+    // arr = ["05:34","06:54","12:19","15:20","17:42","19:12"]
+    return {
+        fajr: arr[0], sunrise: arr[1], dhuhr: arr[2],
+        asr: arr[3], maghrib: arr[4], isha: arr[5], nextFajr: nextArr[0],
+        hDay: arr[6], hMonth: arr[7], hYear: arr[8]
     };
+}
 
-    // يعيد {1:[hh:mm*6],2:[...],...} أو null
-    let loadMonth = function (year) {
-        const fn = yearFile(year);
-        try {
-            const obj = Storage.readJSON(fn);
-            return obj || null;
-        } catch (e) {
-            return null;
-        }
-    };
+function parseHM(s) {
+    // "HH:MM" -> minutes from midnight
+    let a = s.split(":"); return (+a[0]) * 60 + (+a[1]);
+}
 
-    let todayYMD = function (d) {
-        return { y: d.getFullYear(), m: d.getMonth() + 1, day: d.getDate() };
-    };
+function nextPrayerLabel(nowMin, t) {
+    let order = [
+        ["Fajr", parseHM(t.fajr), 30],
+        ["Sunrise", parseHM(t.sunrise), 15],
+        ["Dhuhr", parseHM(t.dhuhr), 30],
+        ["Asr", parseHM(t.asr) + 720, 30],
+        ["Maghrib", parseHM(t.maghrib) + 720, 20],
+        ["Isha", parseHM(t.isha) + 720, 30]
+    ];
+    for (let i = 0; i < order.length; i++) {
+        if ((order[i][1] + order[i][2]) >= nowMin) return order[i];
+    }
+    // إنتهى اليوم: القادمة فجر الغد (للعرض فقط)
+    return ["Fajr", parseHM(t.nextFajr) + 1440];
+}
+// ==== الرسم لواجهة Bangle.js 2 (176x176) ====
+let shownDate = new Date(); // يمكن التنقل باليوم للأمام/الخلف
+let drawTimeout;
+let click = 0;
+function draw() {
+    var x = g.getWidth() / 2;
+    var y = g.getHeight() / 2;
+    g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var timeStr = (" " + (hours % 12 || 12)).substr(-2) + ":" + ("0" + minutes).substr(-2); // Hour and minute
 
-    let getTimesFor = function (d) {
-        const ymd = todayYMD(d);
-        let data = loadMonth(ymd.y);
-        if (!data) return null;
-        let now = new Date();
-        let start = new Date(now.getFullYear(), 0, 0);
-        let diff = now - start;
-        let oneDay = 1000 * 60 * 60 * 24;
-        ymd.day = Math.floor(diff / oneDay);
-        let arr = data[ymd.day];
-        if (!arr) return null;
-        // arr = ["05:34","06:54","12:19","15:20","17:42","19:12"]
-        return {
-            fajr: arr[0], sunrise: arr[1], dhuhr: arr[2],
-            asr: arr[3], maghrib: arr[4], isha: arr[5],
-            hDay: arr[6], hMonth: arr[7], hYear: arr[8]
-        };
-    };
-
-    let parseHM = function (s) {
-        // "HH:MM" -> minutes from midnight
-        const a = s.split(":"); return (+a[0]) * 60 + (+a[1]);
-    };
-
-    let nextPrayerLabel = function (nowMin, t) {
-        const order = [
-            ["Fajr", parseHM(t.fajr)],
-            ["Sunrise", parseHM(t.sunrise)],
-            ["Dhuhr", parseHM(t.dhuhr)],
-            ["Asr", parseHM(t.asr) + 720],
-            ["Maghrib", parseHM(t.maghrib) + 720],
-            ["Isha", parseHM(t.isha) + 720]
-        ];
-        for (let i = 0; i < order.length; i++) {
-            if (order[i][1] >= nowMin) return order[i];
-        }
-        // إنتهى اليوم: القادمة فجر الغد (للعرض فقط)
-        return ["Fajr", parseHM(t.fajr) + 1440];
-    };
-
-    // ==== الرسم لواجهة Bangle.js 2 (176x176) ====
-    let shownDate = new Date(); // يمكن التنقل باليوم للأمام/الخلف
-    let drawTimeout;
-    let draw = function () {
-        var x = g.getWidth() / 2;
-        var y = g.getHeight() / 2;
-        g.reset().clearRect(Bangle.appRect); // clear whole background (w/o widgets)
-        var date = new Date();
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var timeStr = (" " + (hours % 12 || 12)).substr(-2) + ":" + ("0" + minutes).substr(-2); // Hour and minute
-        g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y + 17);
-
-        // var dateStr = require("locale").date(date, 0).toUpperCase()+"\n"+
-        //               require("locale").dow(date, 0).toUpperCase();
-        // g.setFontAlign(0, 0).setFont("6x8", 2).drawString(dateStr, x, y+48);
-
+    let t = getTimesFor(shownDate);
+    let ymd = todayYMD(shownDate);
+    if (!t) {
         g.setFont("6x8", 2);
-        g.setFontAlign(0, -1);
-
-        const t = getTimesFor(shownDate);
-        const title = CITY;
-        g.drawString(title, g.getWidth() / 2, 6);
-
-        const ymd = todayYMD(shownDate);
+        g.drawString("Month data missing!", g.getWidth() / 2, 60);
         g.setFont("6x8", 1);
-        g.drawString(require("locale").date(date, 0).toUpperCase(), g.getWidth() / 2, 25);
-        g.drawString(require("locale").dow(date, 0).substr(0, 3).toUpperCase() + "\t" + t.hYear + "-" + t.hMonth + "-" + ("0" + t.hDay).substr(-2), g.getWidth() / 2, 37);
-
-        if (!t) {
-            g.setFont("6x8", 2);
-            g.drawString("Month data missing!", g.getWidth() / 2, 60);
-            g.setFont("6x8", 1);
-            g.drawString("Install file:", g.getWidth() / 2, 80);
-            g.drawString(yearFile(ymd.y), g.getWidth() / 2, 92);
-            g.drawString("via IDE: Storage.writeJSON(...)", g.getWidth() / 2, 110);
-            return;
-        }
-
+        g.drawString("Install file:", g.getWidth() / 2, 80);
+        g.drawString(yearFile(ymd.y), g.getWidth() / 2, 92);
+        g.drawString("via IDE: Storage.writeJSON(...)", g.getWidth() / 2, 110);
+        return;
+    }
+    if (click == 1) {
         // القائمة
         g.setFont("6x8", 2);
-        y = 36;
-        const rows = [
+        y = 43;
+        let rows = [
             ["Fajr", t.fajr],
             ["Sunrise", t.sunrise],
             ["Dhuhr", t.dhuhr],
@@ -121,43 +103,54 @@ Graphics.prototype.setFontAnton = function (scale) {
             ["Maghrib", t.maghrib],
             ["Isha", t.isha]
         ];
-        // for (let i=0;i<rows.length;i++){
-        //   g.setFontAlign(-1,-1); g.drawString(rows[i][0], 8, y);
-        //   g.setFontAlign(1,-1); g.drawString(rows[i][1], g.getWidth()-8, y);
-        //   y += 20;
-        // }
+        g.setFontAlign(0, 0).setFont("6x8", 2).drawString(require("locale").date(date, 0), g.getWidth() / 2, 32);
+        for (let i = 0; i < rows.length; i++) {
+            g.setFontAlign(-1, -1); g.drawString(rows[i][0], 8, y);
+            g.setFontAlign(1, -1); g.drawString(rows[i][1], g.getWidth() - 8, y);
+            y += 23;
+        }
+        return;
+    }
+    g.setFontAlign(0, 0).setFont("Anton").drawString(timeStr, x, y + 17);
+    g.setFont("6x8", 2);
+    g.setFontAlign(0, -1);
+    g.drawString(require("locale").dow(date, 0).substr(0, 3).toUpperCase() + " " + t.hYear + "-" + t.hMonth + "-" + ("0" + t.hDay).substr(-2), g.getWidth() / 2, 32);
+    // g.drawString(require("locale").dow(date, 0).substr(0, 3).toUpperCase() + "\t" + t.hYear + "-" + t.hMonth + "-" + ("0" + t.hDay).substr(-2), g.getWidth() / 2, 37);
 
-        // الصلاة القادمة
-        const nowMin = hours * 60 + minutes;
-        const np = nextPrayerLabel(nowMin, t);
-        const minsLeft = (np[1] - nowMin + 1440) % 1440;
-
-        g.setFontAlign(0, -1);
-        g.drawLine(5, y + 15, g.getWidth() - 5, y + 15);
+    // الصلاة القادمة
+    y = 36;
+    let nowMin = hours * 60 + minutes;
+    let np = nextPrayerLabel(nowMin, t);
+    let minsLeft = (np[1] - nowMin + 1440) % 1440;
+    let minsAgo = nowMin - np[1];
+    g.setFontAlign(0, -1);
+    g.drawLine(5, y + 15, g.getWidth() - 5, y + 15);
+    if (np[1] > nowMin ){
         g.drawString(np[0] + " in", g.getWidth() / 2, y + 105);
         g.drawString(("0" + Math.floor(minsLeft / 60)).substr(-2) + ":" + ("0" + (minsLeft % 60)).substr(-2), g.getWidth() / 2, y + 121);
-
-        // queue next draw
-        if (drawTimeout) clearTimeout(drawTimeout);
-        drawTimeout = setTimeout(function () {
-            drawTimeout = undefined;
-            draw();
-        }, 60000 - (Date.now() % 60000));
-    };
-
-    // Show launcher when middle button pressed
-    Bangle.setUI({
-        mode: "clock",
-        remove: function () {
-            // Called to unload all of the clock app
-            if (drawTimeout) clearTimeout(drawTimeout);
-            drawTimeout = undefined;
-            delete Graphics.prototype.setFontAnton;
-        },
-        redraw: draw,
-    });
-    // Load widgets
-    Bangle.loadWidgets();
-    draw();
-    setTimeout(Bangle.drawWidgets, 0);
+    }
+    else {
+        g.drawString(np[0] + " since", g.getWidth() / 2, y + 105);
+        g.drawString(minsAgo + " min", g.getWidth() / 2, y + 121);
+    }
+    // queue next draw
+    if (drawTimeout) clearTimeout(drawTimeout);
+    drawTimeout = setTimeout(function () {
+        drawTimeout = undefined;
+        draw();
+    }, 60000 - (Date.now() % 60000));
 }
+setWatch(() => {
+
+    click = click + 1;
+    if (click == 2) {
+        return Bangle.showLauncher();
+    }
+    draw();
+
+}, BTN1, { repeat: true });
+
+// Load widgets
+Bangle.loadWidgets();
+draw();
+setTimeout(Bangle.drawWidgets, 0);
