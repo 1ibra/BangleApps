1094b7c26be819107f973b6fc593a5af151af414
1ibra <82260531+1ibra@users.noreply.github.com>
2025-09-04 20:16:00 +0300

    Update widget.js

--- apps/widid/widget.js
+++ apps/widid/widget.js
@@ -3,7 +3,7 @@
     let settings = require("Storage").readJSON('setting.json', 1);
     g.reset().setFont("6x8", 2);
     let city = settings.city;
-    g.drawString(city.toUpperCase().substr(0,3), this.x,4+this.y);
+    g.drawString(city.toUpperCase().substr(0,3), 3+this.x,4+this.y);
   }
   // add your widget
   WIDGETS["widcity"]={
