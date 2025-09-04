49a4c3df1cd055e2813f6c8682c742331672e790
1ibra <82260531+1ibra@users.noreply.github.com>
2025-09-04 20:13:30 +0300

    Update widget.js

--- apps/widid/widget.js
+++ apps/widid/widget.js
@@ -7,9 +7,8 @@
   }
   // add your widget
   WIDGETS["widcity"]={
-    area:"tr", // tl (top left), tr (top right), bl (bottom left), br (bottom right), be aware that not all apps support widgets at the bottom of the screen
+    area:"tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right), be aware that not all apps support widgets at the bottom of the screen
     width: 35, // how wide is the widget? You can change this and call Bangle.drawWidgets() to re-layout
-    sortorder: 2,
     draw:draw // called to draw the widget
   };
 })();
